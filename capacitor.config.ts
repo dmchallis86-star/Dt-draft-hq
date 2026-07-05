5df01bd2d0f1a1bfbac94173bfc313c238be7356
dmchallis86-star <dmchallis86@gmail.com>
2026-07-05 20:55:16 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'uk.co.dt.drafthq',
   appName: 'D&T Draft HQ',
-  webDir: '.',
+  webDir: 'www',
   server: {
     androidScheme: 'https'
   }
